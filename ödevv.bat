847252972fb9fbac72632e368f259eebe38b2032
Muhammed Yusuf Tekin <muhammedyusuf_tekin20@erdogan.ed.tr>
2021-10-27 16:52:45 +0300

    ödevv

---
+++ ödevv.bat
@@ -0,0 +1,8 @@
+type nul > pc-information.txt
+hostname >> pc-informaton.txt
+color 1 >> pc-informaton.txt
+net start >> pc-informaton.txt
+hostname >> pc-informaton.txt
+ipconfig >> pc-informaton.txt
+wmic diskdrive get size >> pc-informaton.txt
+pause
